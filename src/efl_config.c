8a1ad225402b1d74493711f75d8d425f41b02e62
Jaehwan Kim <jae.hwan.kim@samsung.com>
2016-01-26 09:58:23 +0900

    Initialization
    Change-Id: I579fed187989904fa9181bd524c00b6773ae3bfb

---
+++ src/efl_config.c
@@ -0,0 +1,31 @@
+#include <Elementary.h>
+#include <Ecore_Wayland.h>
+
+static void
+_scale_set(void)
+{
+   char *s = NULL;
+
+   s = getenv("ELM_SCALE");
+   if (!s) elm_config_scale_set(1.0);
+
+   elm_config_save();
+   elm_config_all_flush();
+}
+
+int main(int argc, char **argv)
+{
+	Evas_Object *win;
+
+	elm_init(argc, argv);
+
+	win = elm_win_add(NULL, "config", ELM_WIN_BASIC);
+
+	_scale_set();
+
+	elm_run();
+
+	elm_shutdown();
+
+	return EINA_TRUE;
+}
